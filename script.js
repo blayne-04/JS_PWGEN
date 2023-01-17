b997c5dc73bf9844148e1583e6e9619d29638fb2
blayne-04 <befuller2004@gmail.com>
2023-01-17 14:58:30 -0800

    cleaned up files

--- script.js
+++ script.js
@@ -1,35 +1,43 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword)
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword)
 
 //functions for fetching random characters
 function randomLower(){
-  const lower = 'abcdefghijklmnopqrstuvwxyz';
+  var lower = 'abcdefghijklmnopqrstuvwxyz';
   return lower[Math.floor(Math.random() * lower.length)];
 }
-
+console.log(randomLower())
 function randomUpper(){
-  const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  var upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
   return upper[Math.floor(Math.random() * upper.length)]
 }
 
 function randomNumber(){
-  const numbers = '0123456789'
+  var numbers = '0123456789'
   return numbers[Math.floor(Math.random() * numbers.length)]
 }
 
 function randomSymbol() {
-  const symbols = '!!@#$%^&*(){}[]/,.';
+  var symbols = '!!@#$%^&*(){}[]/,.';
   return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+// object for storing bool values
+var randomFunc = {
+  lower: randomLower,
+  upper: randomUpper,
+  number: randomNumber,
+  symbol: randomSymbol,
 }
